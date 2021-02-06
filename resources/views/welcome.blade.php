cf70630c5f51b74f4ac22efbbc8587cac0a875e2
Emesson Silva <emessontjs@hotmail.com>
2021-02-06 09:40:16 -0300

    primeiro commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/app.css">
+
+        <title>Rookie</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <body>
+       <h1>Rookie</h1>
+       
+    </body>
+    <script src=/js/app.js></script>
+</html>
